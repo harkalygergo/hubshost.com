57c6a9ba39edb239cd7cb19848b958b453179e61
Harkály Gergő <git@harkalygergo.hu>
2023-02-05 00:07:35 +0100

    homepage content

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,14 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class HomepageController extends AbstractController
 {
-	/**
-	* Route('/', methods: ['GET'], name: 'homepage')
-	*/
-	public function homepage(Request $request): Response
-	{
-	        return $this->render('base.html.twig', [
-			'content' => 'unifilter.org',
-	        ]);
-	}
+    /**
+     * Route('/', methods: ['GET'], name: 'homepage')
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('base.html.twig', [
+            'content' => '<h2>A <code>DATAHUB</code> egy ingyenes online univerzális adatbázis, mely több témakörben is biztosít összetett keresési lehetőséget.</h1><h2>Mottónk: <blockquote>Keress tudatosan, szűrj részletesen!</blockquote></h2>
+                <h3>Kategóriák:</h3>
+                <ul>
+                    <li><a href="/hu/watch">Wathces</a></li>
+                </ul>
+
+            ',
+        ]);
+    }
 }
 
